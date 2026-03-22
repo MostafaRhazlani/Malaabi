be2f6c720a40b5eb3e15bf147132806c2a0c76c6
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2026-03-22 00:43:48 +0000

    refactor(hooks): Updated native and web implementations to return { colorScheme, isDark, isLight } for consistent destructuring.

--- mobile-frontend/hooks/use-color-scheme.web.ts
+++ mobile-frontend/hooks/use-color-scheme.web.ts
@@ -1,21 +1,20 @@
 import { useEffect, useState } from 'react';
-import { useColorScheme as useRNColorScheme } from 'react-native';
+import { useColorScheme as useNativeWindColorScheme } from 'nativewind';
 
-/**
- * To support static rendering, this value needs to be re-calculated on the client side for web
- */
 export function useColorScheme() {
+  const { colorScheme } = useNativeWindColorScheme();
   const [hasHydrated, setHasHydrated] = useState(false);
 
   useEffect(() => {
     setHasHydrated(true);
   }, []);
 
-  const colorScheme = useRNColorScheme();
+  const active = colorScheme ?? 'light';
+  const final = hasHydrated ? active : 'light';
 
-  if (hasHydrated) {
-    return colorScheme;
-  }
-
-  return 'light';
+  return {
+    colorScheme: final,
+    isDark: final === 'dark',
+    isLight: final === 'light',
+  };
 }
